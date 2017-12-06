e825adf2da02ed20cd38b6e3d8f96791193c3012
Maxwell Hunt <mhunt@canbtv.com>
2017-12-06 16:52:43 -0500

    Confirmed features: -Playlists (previous version) -Reload on content changes
    Todo:
    -Synchronise content between multiple browser tabs
    -Accept calls from http://url/A0 and so on (will this even keep going that long?)
    -Switch over to https (seriously, will this even be in use that long?)

--- server.js
+++ server.js
@@ -1,10 +1,77 @@
+'use strict';
+/*
+Debug setup
+*/
+const BASIC_DEBUG = 1;
+const DATA_DEBUG = 2;
+const FOCUS_DEBUG = 4;
+const ERROR_DEBUG = 8;
+const WATCHER_DEBUG = 16;
+const WEBSOCK_DEBUG = 32;
+
+var debug = BASIC_DEBUG | WEBSOCK_DEBUG | FOCUS_DEBUG;
+
+function debugLog(flag, msg) {
+	if (debug & flag || flag & ERROR_DEBUG) {
+		console.log(msg);
+	}
+}
+
+/*
+Web server setup
+*/
 var express = require('express');
 var app = express();
 var fs = require('fs'), parseString = require('xml2js').parseString;
 var ejs = require('ejs');
+var server = require('http').createServer();
 var unzip = require('decompress');
 app.set('view engine', 'ejs');
 
+/*
+Directory watching and remote restarting
+*/
+
+var watch = require('watch');
+var WebSocket = require('ws');
+var wss = new WebSocket.Server({server: server});
+wss.broadcast = function broadcast(data) {
+	debugLog(WEBSOCK_DEBUG, 'Broadcast called');
+	wss.clients.forEach(function each(client) {
+		if (client.readyState === WebSocket.OPEN) {
+			debugLog(WEBSOCK_DEBUG, 'Sending ' + data + ' to client');
+			client.send(data);
+		}
+	});
+};
+wss.on('connection', function connection(ws) {
+	debugLog(BASIC_DEBUG, 'Client connected');
+	ws.on('message', function incoming(data) {
+		debugLog(WEBSOCK_DEBUG, 'Received message: ' + data);
+	});
+	ws.on('close', function () {debugLog(BASIC_DEBUG, 'Client disconnected');});
+	ws.send('connected');
+});
+watch.watchTree('/Urchannel/SignalFiles', function (f, curr, prev) {
+	debugLog(WEBSOCK_DEBUG, 'Found change in signal files, triggering reload');
+	wss.broadcast('reload');
+});
+watch.watchTree('/btv/incoming/PriceFiles', function (f, curr, prev) {
+	debugLog(WEBSOCK_DEBUG, 'Found change in price file, triggering reload');
+	wss.broadcast('reload');
+});
+watch.watchTree('/Urchannel/NodePlayer', function (f, curr, prev) {
+	debugLog(WEBSOCK_DEBUG, 'Found change in NodePlayer, triggering reload');
+	wss.broadcast('reload');
+});
+
+
+/*
+Implementation
+*/
+
+// A test based callback trigger.  When the specified variable meets the specified test, perform callback function
+// watchVar is a variable reference.  ChangeTest is a function receiving the watchVar.  Callback is the function to run (without arguments).
 function Watcher(watchVar, ChangeTest, Callback) {
 	this.watchVar = watchVar;
 	this.ChangeTest = ChangeTest;
@@ -16,31 +83,34 @@ function Watcher(watchVar, ChangeTest, Callback) {
 
 	this.SetValue = function(val) {
 		this.watchVar = val;
-		console.log('Set value: ' + this.watchVar);
+		debugLog('Set value: ' + this.watchVar);
 		if (this.ChangeTest(this.watchVar)) {
-			console.log('Doing callback');
+			debugLog(WATCHER_DEBUG, 'Doing callback');
 			this.Callback();
 		}
 	}
 }
 
+// Get price files for use in playback.
+// Not actually used, content dept. producing HTML 5 pages that loads this themselves.
 function getPriceFile() {
-	var priceFile = fs.readFileSync('c:/btv/incoming/PriceFiles/price.xml', 'utf-8', function (err) {
-		if (err) console.log(err);
+	var priceFile = fs.readFileSync('/btv/incoming/PriceFiles/price.xml', 'utf-8', function (err) {
+		if (err) debugLog(ERROR_DEBUG, err);
 	});
 	parseString(priceFile, function(err, result) {
-		if (err) { console.log(err); }
-		//console.log(result);
+		if (err) { debugLog(ERROR_DEBUG, err); }
+		//debugLog(result);
 		priceFile = result.specials;
 	});
-	console.log('Found items:');
+	//debugLog('Found items:');
 	for (item in priceFile) {
 		priceFile[item] = priceFile[item][0].trim();
-		//console.log('|-' + item + ':' + priceFile[item]);
+		//debugLog('|-' + item + ':' + priceFile[item]);
 	}
 	return priceFile;
 }
 
+// Removes a directory, recursively.  Used to clear old extracted zip files for HTML content.
 function rmdirRecursively(path) {
 	if (fs.existsSync(path)) {
 		fs.readdirSync(path).forEach(function(file, index){
@@ -55,14 +125,17 @@ function rmdirRecursively(path) {
 	}
 };
 
+// Content retrieval:  https://127.0.0.1/incoming/Media/<file name> returns the requested file, be it HTML or other.
+// TODO:  Spits out an error if the file doesn't exist, should look into catching that at some point.
+// Note:  Zip file downloads are not supported, as we should never be sending zips to the browser.  Just their contents.
 app.all('/incoming/Media/:path*', function (req, res, next) {
 	switch (req.params[0].slice(req.params[0].lastIndexOf('.'))) {
 		case '.zip':
 		res.send('File request: /incoming/Media/' + req.params.path + req.params[0]);
 		break;
 		case '.html':
-		console.log("Got HTML request: c:/Urchannel/incoming/Media/" + req.params.path + req.params[0]);
-		console.log("Renaming index.html to ejs");
+		debugLog(DATA_DEBUG, "Got HTML request: /Urchannel/incoming/Media/" + req.params.path + req.params[0]);
+		debugLog(DATA_DEBUG, "Renaming index.html to ejs");
 		fs.rename(req.params.path + req.params[0], req.params.path + req.params[0].replace(/\..{0,4}$/i, '.ejs'), function(err) {
 			if (err) {
 				if (err.code !== 'ENOENT') {
@@ -70,11 +143,11 @@ app.all('/incoming/Media/:path*', function (req, res, next) {
 				}
 			}
 		});
-		res.render('c:/urchannel/incoming/Media/'+ req.params.path + req.params[0].replace(/\..{0,4}$/i, '.ejs'), getPriceFile()); 
+		res.render('/urchannel/incoming/Media/'+ req.params.path + req.params[0].replace(/\..{0,4}$/i, '.ejs'), getPriceFile()); 
 		break;
 		default:
 		var options = {
-			root: 'c:/Urchannel/incoming/Media/',
+			root: '/Urchannel/incoming/Media/',
 			headers: {
 				'x-timestamp': Date.now(),
 				'x-sent': true
@@ -86,54 +159,62 @@ app.all('/incoming/Media/:path*', function (req, res, next) {
 					next(err);
 				}
 			} else {
-				console.log('Served file: ' + req.params.path + req.params[0]);
+				debugLog(DATA_DEBUG, 'Served file: ' + req.params.path + req.params[0]);
 			}
 		});
 	}
 });
+
+// Main block
+// Returns a base HTML page using EJS to populate fields.
+// Parses the appropriate show file for necessary region data and sets up specified regions as DIV tags.
 app.get('/', function (req, res) {
 	var showFileName;
 
-	//console.log(priceFile);
+	//debugLog(priceFile);
 
-	fs.readdir('C://urchannel//SignalFiles', function(err, files) {
-		console.log('Files: ' + files);
+	// Find all the signal files, the _show[r0c0] one will be the one we want to load
+	fs.readdir('/urchannel/SignalFiles', function(err, files) {
+		debugLog(DATA_DEBUG, 'Files: ' + files);
 		for (var x = 0; x < files.length; x++ ) {
 			if (files[x].search(/_show\[r0c0\]/) >= 0) {
 				showFileName = files[x].replace("_show\[r0c0\].sgf", ".show");
-				console.log("Found signal file: " + files[x] + " - Renaming to " + showFileName);
+				debugLog(BASIC_DEBUG, "Found signal file: " + files[x] + " - Renaming to " + showFileName);
 				break;
 			}
 		}
 
-		fs.readFile('C:/Urchannel/incoming/ShowFiles/' + showFileName, 'utf-8', function (err, data) {
+		// Load the specified show file, extracting the item info from the first (and hopefully only!) region that has a date range including today
+		fs.readFile('/Urchannel/incoming/ShowFiles/' + showFileName, 'utf-8', function (err, data) {
 			var showFile = JSON.parse(data);
 			var regions = [];
 			var htmlOut = '';
 			//var completeRegions = new Watcher(0, function(val) {return val >= regions.length;}, function() {res.render('index', {htmlOut: htmlOut});});
 			var itemCount = 0;
-			for (region of showFile.Regions) {
-				console.log('Region: ' + region.Name);
-				for (playlist of region.Playlists) {
-					console.log(playlist.Name);
-					console.log('date string:'+playlist.StartTime.slice(6,-2));
+			for (var region of showFile.Regions) {
+				debugLog(BASIC_DEBUG, 'Region: ' + region.Name);
+				// Iterate over the region entries looking for the current playlist
+				for (var playlist of region.Playlists) {
+					debugLog(DATA_DEBUG, playlist.Name);
+					debugLog(DATA_DEBUG, 'date string:'+playlist.StartTime.slice(6,-2));
 					var today = new Date();
 					var startDate = new Date(parseInt(playlist.StartTime.slice(6,-2)));
 					var endDate = new Date(parseInt(playlist.EndTime.slice(6,-2)));
-					console.log('startDate [' + startDate + ']\ntoday['+today+']\nendDate ['+endDate+']');
+					debugLog(BASIC_DEBUG, 'startDate [' + startDate + ']\ntoday['+today+']\nendDate ['+endDate+']');
 					if (today > startDate && today < endDate) {
-/*						console.log('File name = ' + playlist.Items[0].Source);
-console.log('mediaName = ' + playlist.Items[0].Name.replace(/[\&\s]/g,'').replace(/\..{0,4}$/i, ''));*/
-regions.push({
-	"x": (region.CanvasLeft / 1280) * 100,
-	"y": (region.CanvasTop / 720) * 100,
-	"w": (region.Width / 1280) * 100,
-	"h": (region.Height /720) * 100,
-	"name": region.Name,
-	"mediaName": playlist.Name.replace(/[\&\s]/g,''),
-	"mediaInfo": []
-});
-for (item of playlist.Items) {
+						/*debugLog('File name = ' + playlist.Items[0].Source);
+						debugLog('mediaName = ' + playlist.Items[0].Name.replace(/[\&\s]/g,'').replace(/\..{0,4}$/i, ''));*/
+						regions.push({
+							"x": (region.CanvasLeft / 1280) * 100,
+							"y": (region.CanvasTop / 720) * 100,
+							"w": (region.Width / 1280) * 100,
+							"h": (region.Height /720) * 100,
+							"name": region.Name,
+							"mediaName": playlist.Name.replace(/[\&\s]/g,''),
+							"mediaInfo": []
+						});
+						// Push a bunch of items onto the stack for this region for future playback
+						for (var item of playlist.Items) {
 							/*var itemType = item.AssetType;
 							switch (itemType) {
 								case 'Movie':
@@ -151,24 +232,27 @@ for (item of playlist.Items) {
 				}
 				itemCount++;
 			}
-			console.log('Found ' + itemCount + ' items to play');
+			debugLog(BASIC_DEBUG, 'Found ' + itemCount + ' items to play');
 			var completeRegions = new Watcher(0, function(val) {return val >= itemCount;}, function() {res.render('index', {htmlOut: htmlOut});});
 			var renames = [];
+
+			// Start building up HTML tags for the region <div> tags and the playlist <li> tags.
+			// Also extract any zip files (clearing out old data) for future use
 			for (var x = 0; x < regions.length; x++) {
-				console.log("region["+x+"]: " + regions[x].x + "," + regions[x].y + " - " + regions[x].w + "," + regions[x].h + " - name: " + regions[x].name + '| mediaName: ' + regions[x].mediaName + ' - mediaPath: ' + JSON.stringify(regions[x].mediaInfo, null, 3));
-				var positioningStuff = 'position:absolute;top:' + regions[x].y + '%;left:' + regions[x].x+ '%;width:' + regions[x].w + '%;height:' + regions[x].h +'%;';
+				debugLog(BASIC_DEBUG, "region["+x+"]: " + regions[x].x + "," + regions[x].y + " - " + regions[x].w + "," + regions[x].h + " - name: " + regions[x].name);
+				var positioningStuff = 'position:absolute;top:' + regions[x].y + '%;left:' + regions[x].x+ '%;width:' + regions[x].w + '%;height:' + regions[x].h +'%;z-index:'+x+';';
 				/*var divStart*/ htmlOut = htmlOut + '<div id="'+regions[x].name+'" style="' +positioningStuff+'"></div>\n';
 				htmlOut = htmlOut + '<div id="'+regions[x].name+'-playlist" style="display:none;">\n'
 				for (var y = 0; y < regions[x].mediaInfo.length ; y++) {
 					var jsonData = {"source": "/incoming" + regions[x].mediaInfo[y].source, "type" : regions[x].mediaInfo[y].type, "Duration" : regions[x].mediaInfo[y].duration};
-					console.log('Configured new jsonData: ' + JSON.stringify(jsonData));
+					debugLog(DATA_DEBUG, 'Configured new jsonData: ' + JSON.stringify(jsonData));
 					if (regions[x].mediaInfo[y].type === 'zip') {
 						jsonData.source = jsonData.source.replace(/\..{0,4}$/i,'');
-						if (fs.existsSync('c:/Urchannel/incoming' + regions[x].mediaInfo[y].source.replace(/\..{0,4}$/i,''))) {
-							console.log('Deleting current unzipped folder');
-							rmdirRecursively('c:/Urchannel/incoming' + regions[x].mediaInfo[y].source.replace(/\..{0,4}$/i,''));
-							console.log('Complete');
-							unzip('c:/Urchannel/incoming' + regions[x].mediaInfo[y].source, 'c:/Urchannel/incoming/Media/', {
+						if (fs.existsSync('/Urchannel/incoming' + regions[x].mediaInfo[y].source.replace(/\..{0,4}$/i,''))) {
+							debugLog(DATA_DEBUG, 'Deleting current unzipped folder');
+							rmdirRecursively('/Urchannel/incoming' + regions[x].mediaInfo[y].source.replace(/\..{0,4}$/i,''));
+							debugLog(DATA_DEBUG, 'Complete');
+							unzip('/Urchannel/incoming' + regions[x].mediaInfo[y].source, '/Urchannel/incoming/Media/', {
 								map: file => {
 									file.path = file.path.replace(/\.html$/i,'.ejs');
 									return file;
@@ -181,17 +265,17 @@ for (item of playlist.Items) {
 									indexPath = res[0].path;
 								}
 							}).catch(err => {
-								console.log('Error in unzip process: ' + err.stack);
+								debugLog(BASIC_DEBUG, 'Error in unzip process: ' + err.stack);
 							});
 						}
 					}
 					/*switch (regions[x].mediaPath.slice(regions[x].mediaPath.lastIndexOf('.'))) {
 						case '.zip':
-						console.log('Zip stuff: ' + regions[x].mediaPath);
+						debugLog('Zip stuff: ' + regions[x].mediaPath);
 						if (fs.existsSync('c:/Urchannel/incoming' + regions[x].mediaPath.replace(/\..{0,4}$/i,''))) {
-							console.log('Deleting current unzipped folder');
+							debugLog('Deleting current unzipped folder');
 							rmdirRecursively('c:/Urchannel/incoming' + regions[x].mediaPath.replace(/\..{0,4}$/i,''));
-							console.log('Complete');
+							debugLog('Complete');
 						}
 						//htmlOut = htmlOut + divStart + '<iframe scrolling="no" src="/incoming' + regions[x].mediaPath.replace(/\..{0,4}$/i, '') + '/index.html' +'" style="border:none;" width="100%" height="100%"></iframe></div>\n';
 						htmlOut = htmlOut + '<li class>' + JSON.stringify(jsonData) + '</li>'
@@ -208,9 +292,9 @@ for (item of playlist.Items) {
 								indexPath = res[0].path;
 							}
 							completeRegions.SetValue(completeRegions.GetValue()+1);
-							console.log('Added iframe for zip file: ' + indexPath.replace(/\/.*?$/i, '/') + 'index.html');
+							debugLog('Added iframe for zip file: ' + indexPath.replace(/\/.*?$/i, '/') + 'index.html');
 						}).catch(err => {
-							console.log('Error in unzip process: ' + err.stack);
+							debugLog('Error in unzip process: ' + err.stack);
 						});
 						break;
 						case '.html':
@@ -231,15 +315,18 @@ for (item of playlist.Items) {
 				htmlOut = htmlOut + '<script>var ' + regions[x].name + ' = new RegionPlayer("' + regions[x].name + '");</script>\n';
 				completeRegions.SetValue(completeRegions.GetValue()+1);
 			}
-/*			if (completeRegions == regions.length) {
+			/*if (completeRegions == regions.length) {
 				res.render('index', {htmlOut: htmlOut});
 			} else {
-				console.log('Reached render early.');
+				debugLog('Reached render early.');
 			}*/
 		});
 });
 
 });
-app.listen(3000, function () {
-	console.log('Starting:  *:3000');
+// Get this party started
+//app.listen(3000, function () {
+server.on('request', app);
+server.listen(3000, function() {
+	debugLog(BASIC_DEBUG, 'Starting http server:  *:3000');
 })
